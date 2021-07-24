978ee0ddb2eeca204889b017e645a1d7eeb9dd53
Caseywalker <casey.walker118@gmail.com>
2021-07-23 19:14:52 -0500

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner
+{
+  class Building
+  {
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+    public double Volume { get; private set; } 
+
+    private string _designer;
+    private DateTime _dateConstructed;
+    private string _address;
+    private string _owner;
+
+    public Building(string address, int stories = 1, double width = 1, double depth = 1)
+    {
+      _address = address;
+      Stories = stories;
+      Width = width;
+      Depth = depth;
+      Volume = Width * Depth * (3 * Stories);
+    }
+
+    public void Test()
+    {
+      Console.WriteLine(Volume);
+    }
+
+    public void GetVolume()
+    {
+      Volume = Width * Depth * (3 * Stories);
+    }
+
+    public void Construct()
+    {
+      _dateConstructed = DateTime.Now;
+      _designer = "C.W Inc";
+    }
+    
+    public void Purchase(string name)
+    {
+      _owner = name;
+      Console.WriteLine($"{_address}--------------------");
+      Console.WriteLine($"Designed by {_designer}");
+      Console.WriteLine($"Constructed on {_dateConstructed}");
+      Console.WriteLine($"Owned by {name}");
+      Console.WriteLine($"{Volume} cubic meters of space");
+    }
+
+  }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UrbanPlanner
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Urban Planner");
+      var buildingOne = new Building("902 Wildrose", 1, 100, 80);
+      var buildingTwo = new Building("505 Main", 3, 200, 60);
+      var buildingThree = new Building("2 Broad", 2, 100, 40);
+      var buildingFour = new Building("501 Commerce", 10, 250, 100);
+
+      buildingOne.Construct();
+      buildingTwo.Construct();
+      buildingThree.Construct();
+      buildingFour.Construct();
+
+      buildingOne.Purchase("Casey");
+      buildingTwo.Purchase("Ryder");
+      buildingThree.Purchase("Tiny");
+      buildingFour.Purchase("Mike");
+
+      
+    }
+  }
+}
